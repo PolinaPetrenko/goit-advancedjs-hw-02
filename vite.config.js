13351f0317714dc255a3f35eb5a3f46d2c112f11
Polina Petrenko <147924269+PolinaPetrenko@users.noreply.github.com>
2025-01-10 23:51:47 +0100

    Added base configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
+
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-advancedjs-hw-02/', 
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
