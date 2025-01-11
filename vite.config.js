0998431b6572276917dea67b43b27c673a9a91d2
Polina Petrenko <147924269+PolinaPetrenko@users.noreply.github.com>
2025-01-11 01:00:58 +0100

    hw2

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-
 export default defineConfig(({ command }) => {
   return {
-    base: '/goit-advancedjs-hw-02/', 
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
